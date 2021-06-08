7d497f6bd3a600b57f66dff1e5076eb02e8d27bf
suj6757 <85167235+suj6757@users.noreply.github.com>
2021-06-08 17:05:11 +0900

    Update Chart.js

--- client/src/components/body/Chart.js
+++ client/src/components/body/Chart.js
@@ -4,7 +4,7 @@ import ReactApexChart from 'react-apexcharts';
 class Chart extends React.Component {
     render() {
         return <ReactApexChart options={this.props.options} series={this.props.series} type={this.props.type} height={350} />;
-        /*master에서 테스트하느라 수정해봄*/
+        /*master에서 테스트하느라 수정해봄2*/
     }
 }
 
